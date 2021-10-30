12edb0c866de0309397abf26a8dc8b4b4fa0166d
TheValik <90153132+TheValik@users.noreply.github.com>
2021-10-30 15:51:17 +0300

    pidgotovka JS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,92 @@
+// вставка на сайт з лабами
+
+window.addEventListener('click', function(event){
+
+  if(event.target.hasAttribute('data-lab')){
+    console.log("Klik Laba");
+    // Видаляється опис лаби на сторінці
+    document.querySelector('.dell').remove();
+
+    // знаходимо частину коду з класом "vstavka" куди будемо вставляти
+    const mistceVstavky =  document.querySelector('.vstavka');
+    // перевірка на кнопку яку натискають
+    var a=0
+    if(event.target.dataset.lab ==="1"){
+      a=1;
+    }
+    if(event.target.dataset.lab ==="2"){
+      a=2;
+    }
+    if(event.target.dataset.lab ==="3"){
+      a=3;
+    }
+    if(event.target.dataset.lab ==="4"){
+      a=4;
+    }
+    console.log(a)
+    //шаблон
+    const shablon = `<div class="block2 dell" id="labscontent">
+    <div class="flexel" id="names">
+        <div class="flexel2" id="labname">
+            <h1>Laba hahaha (Зробити щось там)</h1>
+        </div>
+        <div class="flexel2" id="labstatus">
+            <h1>Час роботи вийшов</h1>
+        </div>
+        <div class="flexel2" id="labdeadline">
+            <h1>До 01.01.2001 1:00</h1>
+        </div>
+    </div>
+    <div class="differ"></div>
+    <div class="labbody">
+        <div class="flexel" id=labinfo>
+            <h2>Опис роботи:</h2>
+            Далi крутий опис Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sequi consequatur veniam voluptas nostrum? Beatae atque earum, vel, iure asperiores quas nesciunt eaque sed laboriosam enim, tempore quos debitis qui.
+            <ol>
+                <li>zavdanna</li>
+                <li>zavdanna</li>
+                <li>zavdanna</li>
+                <li>zavdanna</li>
+                <li>zavdanna</li>
+                <li>zavdanna</li>
+            </ol>
+        </div>
+        <div class="" id="labinteract">
+            <div class="insidelabinteract">
+                <div class="flexellabint" id="labdownload">
+                    <button class="downloadbutton"><i class="labdownload"><img class="downloadimg" src="img/download.png">  Download</i></button>
+                </div>
+                <div class="flexellabint" id="labyoutube">
+                    <iframe class="labvideo" src="https://www.youtube.com/embed/9WjXGsymT5M" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="insidelabinteract">
+                <div class="flexellabint" id="labgit">
+                    <a href="https://github.com/RomanMykolaichuk/wd2021/tree/LectionFetchAPI" target="_blank"><img class="labGitHub" src="img/GitHub.png"></a>
+                </div>
+                <div class="flexellabint" id="labscreen">
+                    <img class="labscreenimg" src="img/screen.png">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+    
+</div>`
+
+    
+    mistceVstavky.insertAdjacentHTML('beforeend', shablon)
+
+  }
+
+})
+
+
+
+
+
+//----------------------------------------------------------------------------------
 // to see a calendar with weeks starts by Monday look here: https://codepen.io/jacknumber/pen/RWLyQW
 
 function calendar(params) {
